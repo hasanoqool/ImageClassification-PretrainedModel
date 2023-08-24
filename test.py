934570a33c4838ca75dc77e5aab40274542cba59
hasanoqool <hasanoqool98@gmail.com>
2023-08-24 13:11:18 +0300

    test

---
+++ test.py
@@ -0,0 +1,13 @@
+"""
+CONTACT:
+Hasan Al-Oqool : https://www.linkedin.com/in/hasanoqool/
+"""
+
+#===============Import the required libraries===============
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+from keras import Input
+from keras.datasets import mnist #0-9 digits (10 classes) 28*28*1CH
+from keras.layers import Dense
+from keras.models import Model, Sequential
+#===============Import the required libraries===============

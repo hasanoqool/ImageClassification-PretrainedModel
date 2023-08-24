e9bd3acc1bef52be703d89add72811222bae4889
hasanoqool <hasanoqool98@gmail.com>
2023-08-24 14:12:44 +0300

    Creating a binary classifier to detect smiles

--- detect_smiles.py
+++ detect_smiles.py
@@ -10,7 +10,7 @@ import glob
 import numpy as np
 from sklearn.model_selection import train_test_split
 from keras.layers import *
-from tensorflow.keras.preprocessing.image import *
+from keras.preprocessing.image import load_img, img_to_array
 #===============Import the required libraries===============
 
 
@@ -29,5 +29,18 @@ def load_images_labels(image_paths):
     labels = []    
 
     for image_path in image_paths:
-        image = loa
+
+        image = load_img(image_path, target_size=(32,32), color_mode="grayscale")
+        image = img_to_array(image)
+        label = image_path.split(os.path.sep)[-2]
+        label = "positive" in label
+        label = float(label)
+
+        images.append(image)
+        labels.append(label)
+    
+    return np.array(images), np.array(labels)
 # print(load_images_labels.__doc__)
+
+s = float("post" in "post")
+print(s)

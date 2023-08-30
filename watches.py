1a1a7cc5dfa23023c2b96115736e3c59e028aef6
hasanoqool <hasanoqool98@gmail.com>
2023-08-30 14:47:26 +0300

    ceating a multi-label classifier to label watches

---
+++ watches.py
@@ -0,0 +1,16 @@
+"""
+CONTACT:
+Hasan Al-Oqool : https://www.linkedin.com/in/hasanoqool/
+"""
+
+#===============Import the required libraries===============
+import os
+import pathlib
+import glob
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras.layers import *
+from keras.models import Sequential
+from keras.preprocessing.image import load_img, img_to_array
+import matplotlib.pyplot as plt
+#===============Import the required libraries===============

a47b41f619da5210208d695974be232976a06cc6
hasanoqool <hasanoqool98@gmail.com>
2023-09-06 16:28:44 +0300

    classifying images with a pre-trained network using the TF Hub

--- pretrained-tf.py
+++ pretrained-tf.py
@@ -6,7 +6,20 @@ Hasan Al-Oqool : https://www.linkedin.com/in/hasanoqool/
 #===============Import the required libraries===============
 import matplotlib.pyplot as plt
 import numpy as np
-from keras.applications import imagenet_utils
-from keras.applications.inception_v3 import *
+import tensorflow_hub as hub
+from keras.models import Sequential
 from keras.preprocessing.image import *
+from keras.utils import get_file
 #===============Import the required libraries===============
+
+
+def model():
+    """
+    * Read ResNetV2152 model weights through provided link.
+        
+    * Return:
+        - Sequential model.
+    """
+    url = ("https://tfhub.dev/google/imagenet/resnet_v2_152/classification/4")
+    model = Sequential()
+    model.add(hub.KerasLayer(url, input_shape = (224, 224, 3)))

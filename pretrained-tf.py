71a27eb938b5651efb33053726b70930a2971b36
hasanoqool <hasanoqool98@gmail.com>
2023-09-06 16:01:22 +0300

    classifying images with a pre-trained network using the TF Hub

---
+++ pretrained-tf.py
@@ -0,0 +1,12 @@
+"""
+CONTACT:
+Hasan Al-Oqool : https://www.linkedin.com/in/hasanoqool/
+"""
+
+#===============Import the required libraries===============
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.applications import imagenet_utils
+from keras.applications.inception_v3 import *
+from keras.preprocessing.image import *
+#===============Import the required libraries===============

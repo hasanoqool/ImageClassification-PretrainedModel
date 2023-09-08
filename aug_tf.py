0173f2d06b5d0ee6d664c7605015eb30970eae2b
hasanoqool <hasanoqool98@gmail.com>
2023-09-08 13:36:51 +0300

    Using data augmentation to improve performance with TF

---
+++ aug_tf.py
@@ -0,0 +1,19 @@
+"""
+CONTACT:
+Hasan Al-Oqool : https://www.linkedin.com/in/hasanoqool/
+"""
+
+#===============Import the required libraries===============
+import os
+import pathlib
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow_docs as tfdocs
+import tensorflow_docs.plots
+from glob import glob
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+from keras.layers import *
+from keras.models import Model
+from keras.preprocessing.image import *
+#===============Import the required libraries===============
